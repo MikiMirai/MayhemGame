9733f1374cd7e2a0496f0287c71d71a103c28e7e
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-10-22 20:12:26 +0300

    GameManager system to keep track of certain things
    GameManager system will keeps track of things like gold(currency), ammo, weapons and other things in the future.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentGold;
+    public TextMeshProUGUI goldText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddGold(int goldToAdd)
+    {
+        currentGold += goldToAdd;
+        goldText.text = "Gold: " + currentGold;
+    }
+}
