bcc2a64d1a4e9d19dda8e6704c1481d7ec0b4939
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-10-22 20:12:48 +0300

    Script to tell the GameManager to add gold to the player

---
+++ Assets/Scripts/GoldPickup.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPickup : MonoBehaviour
+{
+    public int value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().AddGold(value);
+
+            Destroy(gameObject);
+        }
+    }
+}
