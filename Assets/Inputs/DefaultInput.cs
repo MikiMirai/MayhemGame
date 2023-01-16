3fa9c185ee05b9ef4d599cef5ab9252a7febc360
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-01-16 22:17:34 +0200

    Fixed some names.

--- Assets/Inputs/DefaultInput.cs
+++ Assets/Inputs/DefaultInput.cs
@@ -55,6 +55,15 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                     ""initialStateCheck"": false
                 },
                 {
+                    ""name"": ""Fire"",
+                    ""type"": ""Button"",
+                    ""id"": ""211659f6-57bc-45d9-b50e-5e6d2b67fb09"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
                     ""name"": ""Aim"",
                     ""type"": ""Button"",
                     ""id"": ""19b8be20-bb66-4981-8ddd-2b61ef3521f5"",
@@ -64,9 +73,9 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                     ""initialStateCheck"": false
                 },
                 {
-                    ""name"": ""Fire1"",
+                    ""name"": ""Reload"",
                     ""type"": ""Button"",
-                    ""id"": ""211659f6-57bc-45d9-b50e-5e6d2b67fb09"",
+                    ""id"": ""725d8363-474c-41d9-9a6b-2225b0291006"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """",
@@ -109,16 +118,7 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                     ""initialStateCheck"": false
                 },
                 {
-                    ""name"": ""Reload"",
-                    ""type"": ""Button"",
-                    ""id"": ""725d8363-474c-41d9-9a6b-2225b0291006"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
-                },
-                {
-                    ""name"": ""SwithWeaponUp"",
+                    ""name"": ""SwitchWeapon"",
                     ""type"": ""Value"",
                     ""id"": ""07bd8b92-faf7-43f7-b26f-30c8330ff2a6"",
                     ""expectedControlType"": ""Axis"",
@@ -207,23 +207,12 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": """",
-                    ""id"": ""b744a326-62a8-4046-a554-091c12a278fc"",
-                    ""path"": ""<Mouse>/rightButton"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Aim"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
                     ""id"": ""fb9d722b-9b9b-4c13-8746-8be771361bba"",
                     ""path"": ""<Mouse>/leftButton"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""Fire1"",
+                    ""action"": ""Fire"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 },
@@ -284,6 +273,17 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": """",
+                    ""id"": ""1f18239b-e6e1-45e5-8bfb-b0854698863b"",
+                    ""path"": ""<Mouse>/scroll"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SwitchWeapon"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
                     ""id"": ""bdb08119-316a-4931-86dd-42bc15ec5656"",
                     ""path"": ""<Keyboard>/r"",
                     ""interactions"": """",
@@ -295,12 +295,12 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": """",
-                    ""id"": ""1f18239b-e6e1-45e5-8bfb-b0854698863b"",
-                    ""path"": ""<Mouse>/scroll"",
+                    ""id"": ""b744a326-62a8-4046-a554-091c12a278fc"",
+                    ""path"": ""<Mouse>/rightButton"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""SwithWeaponUp"",
+                    ""action"": ""Aim"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -314,14 +314,14 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
         m_Character_Movement = m_Character.FindAction("Movement", throwIfNotFound: true);
         m_Character_View = m_Character.FindAction("View", throwIfNotFound: true);
         m_Character_Jump = m_Character.FindAction("Jump", throwIfNotFound: true);
+        m_Character_Fire = m_Character.FindAction("Fire", throwIfNotFound: true);
         m_Character_Aim = m_Character.FindAction("Aim", throwIfNotFound: true);
-        m_Character_Fire1 = m_Character.FindAction("Fire1", throwIfNotFound: true);
+        m_Character_Reload = m_Character.FindAction("Reload", throwIfNotFound: true);
         m_Character_Sprinting = m_Character.FindAction("Sprinting", throwIfNotFound: true);
         m_Character_SprintReleased = m_Character.FindAction("SprintReleased", throwIfNotFound: true);
         m_Character_Crouch = m_Character.FindAction("Crouch", throwIfNotFound: true);
         m_Character_Prone = m_Character.FindAction("Prone", throwIfNotFound: true);
-        m_Character_Reload = m_Character.FindAction("Reload", throwIfNotFound: true);
-        m_Character_SwithWeaponUp = m_Character.FindAction("SwithWeaponUp", throwIfNotFound: true);
+        m_Character_SwitchWeapon = m_Character.FindAction("SwitchWeapon", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -384,14 +384,14 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
     private readonly InputAction m_Character_Movement;
     private readonly InputAction m_Character_View;
     private readonly InputAction m_Character_Jump;
+    private readonly InputAction m_Character_Fire;
     private readonly InputAction m_Character_Aim;
-    private readonly InputAction m_Character_Fire1;
+    private readonly InputAction m_Character_Reload;
     private readonly InputAction m_Character_Sprinting;
     private readonly InputAction m_Character_SprintReleased;
     private readonly InputAction m_Character_Crouch;
     private readonly InputAction m_Character_Prone;
-    private readonly InputAction m_Character_Reload;
-    private readonly InputAction m_Character_SwithWeaponUp;
+    private readonly InputAction m_Character_SwitchWeapon;
     public struct CharacterActions
     {
         private @DefaultInput m_Wrapper;
@@ -399,14 +399,14 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
         public InputAction @Movement => m_Wrapper.m_Character_Movement;
         public InputAction @View => m_Wrapper.m_Character_View;
         public InputAction @Jump => m_Wrapper.m_Character_Jump;
+        public InputAction @Fire => m_Wrapper.m_Character_Fire;
         public InputAction @Aim => m_Wrapper.m_Character_Aim;
-        public InputAction @Fire1 => m_Wrapper.m_Character_Fire1;
+        public InputAction @Reload => m_Wrapper.m_Character_Reload;
         public InputAction @Sprinting => m_Wrapper.m_Character_Sprinting;
         public InputAction @SprintReleased => m_Wrapper.m_Character_SprintReleased;
         public InputAction @Crouch => m_Wrapper.m_Character_Crouch;
         public InputAction @Prone => m_Wrapper.m_Character_Prone;
-        public InputAction @Reload => m_Wrapper.m_Character_Reload;
-        public InputAction @SwithWeaponUp => m_Wrapper.m_Character_SwithWeaponUp;
+        public InputAction @SwitchWeapon => m_Wrapper.m_Character_SwitchWeapon;
         public InputActionMap Get() { return m_Wrapper.m_Character; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -425,12 +425,15 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                 @Jump.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnJump;
                 @Jump.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnJump;
                 @Jump.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnJump;
+                @Fire.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnFire;
+                @Fire.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnFire;
+                @Fire.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnFire;
                 @Aim.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnAim;
                 @Aim.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnAim;
                 @Aim.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnAim;
-                @Fire1.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnFire1;
-                @Fire1.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnFire1;
-                @Fire1.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnFire1;
+                @Reload.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnReload;
+                @Reload.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnReload;
+                @Reload.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnReload;
                 @Sprinting.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSprinting;
                 @Sprinting.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSprinting;
                 @Sprinting.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSprinting;
@@ -443,12 +446,9 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                 @Prone.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnProne;
                 @Prone.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnProne;
                 @Prone.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnProne;
-                @Reload.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnReload;
-                @Reload.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnReload;
-                @Reload.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnReload;
-                @SwithWeaponUp.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSwithWeaponUp;
-                @SwithWeaponUp.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSwithWeaponUp;
-                @SwithWeaponUp.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSwithWeaponUp;
+                @SwitchWeapon.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSwitchWeapon;
+                @SwitchWeapon.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSwitchWeapon;
+                @SwitchWeapon.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnSwitchWeapon;
             }
             m_Wrapper.m_CharacterActionsCallbackInterface = instance;
             if (instance != null)
@@ -462,12 +462,15 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                 @Jump.started += instance.OnJump;
                 @Jump.performed += instance.OnJump;
                 @Jump.canceled += instance.OnJump;
+                @Fire.started += instance.OnFire;
+                @Fire.performed += instance.OnFire;
+                @Fire.canceled += instance.OnFire;
                 @Aim.started += instance.OnAim;
                 @Aim.performed += instance.OnAim;
                 @Aim.canceled += instance.OnAim;
-                @Fire1.started += instance.OnFire1;
-                @Fire1.performed += instance.OnFire1;
-                @Fire1.canceled += instance.OnFire1;
+                @Reload.started += instance.OnReload;
+                @Reload.performed += instance.OnReload;
+                @Reload.canceled += instance.OnReload;
                 @Sprinting.started += instance.OnSprinting;
                 @Sprinting.performed += instance.OnSprinting;
                 @Sprinting.canceled += instance.OnSprinting;
@@ -480,12 +483,9 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
                 @Prone.started += instance.OnProne;
                 @Prone.performed += instance.OnProne;
                 @Prone.canceled += instance.OnProne;
-                @Reload.started += instance.OnReload;
-                @Reload.performed += instance.OnReload;
-                @Reload.canceled += instance.OnReload;
-                @SwithWeaponUp.started += instance.OnSwithWeaponUp;
-                @SwithWeaponUp.performed += instance.OnSwithWeaponUp;
-                @SwithWeaponUp.canceled += instance.OnSwithWeaponUp;
+                @SwitchWeapon.started += instance.OnSwitchWeapon;
+                @SwitchWeapon.performed += instance.OnSwitchWeapon;
+                @SwitchWeapon.canceled += instance.OnSwitchWeapon;
             }
         }
     }
@@ -495,13 +495,13 @@ public partial class @DefaultInput : IInputActionCollection2, IDisposable
         void OnMovement(InputAction.CallbackContext context);
         void OnView(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnFire(InputAction.CallbackContext context);
         void OnAim(InputAction.CallbackContext context);
-        void OnFire1(InputAction.CallbackContext context);
+        void OnReload(InputAction.CallbackContext context);
         void OnSprinting(InputAction.CallbackContext context);
         void OnSprintReleased(InputAction.CallbackContext context);
         void OnCrouch(InputAction.CallbackContext context);
         void OnProne(InputAction.CallbackContext context);
-        void OnReload(InputAction.CallbackContext context);
-        void OnSwithWeaponUp(InputAction.CallbackContext context);
+        void OnSwitchWeapon(InputAction.CallbackContext context);
     }
 }
