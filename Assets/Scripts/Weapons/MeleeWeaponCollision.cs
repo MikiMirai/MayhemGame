470c92db97d52e490e674c647757881f7c0730e1
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-02-09 17:25:06 +0200

    Testing backup.

--- Assets/Scripts/Weapons/MeleeWeaponCollision.cs
+++ Assets/Scripts/Weapons/MeleeWeaponCollision.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeWeaponCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public WeaponController m_WeaponController;
+    public GameObject m_HitParticle;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Enemy")
+        {
+
+        }
     }
 }
