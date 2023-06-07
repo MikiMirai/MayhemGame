394dd8894b78fb9b875a2f149ea290f5bbd54d37
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-07 21:58:43 +0300

    Testing new Third Person camera.

---
+++ Assets/Scripts/Player/ThirdPersonCam.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        
+    }
+}
