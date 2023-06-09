4bebbcc6e7c829c7f9b821bb32a229464032a82c
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-09 22:31:13 +0300

    feat: Overhaul of the player movement.

---
+++ Assets/Scripts/Player/Third Person/PlayerManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    InputManager inputManager;
+    CameraManager cameraManager;
+    PlayerLocomotion playerLocomotion;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+        cameraManager = FindObjectOfType<CameraManager>();
+        playerLocomotion = GetComponent<PlayerLocomotion>();
+    }
+
+    private void Update()
+    {
+        inputManager.HandleAllInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        playerLocomotion.HandleAllMovement();
+    }
+
+    private void LateUpdate()
+    {
+        cameraManager.HandleAllCameraMovement();
+    }
+}
