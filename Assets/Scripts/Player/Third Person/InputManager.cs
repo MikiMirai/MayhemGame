46b5f48c0dd3c42aa9894a206f20822a27f180f8
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-09 22:30:17 +0300

    feat: Adding new input manager.

---
+++ Assets/Scripts/Player/Third Person/InputManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    DefaultInput playerControls;
+    AnimatorManager animatorManager;
+
+    public Vector2 movementInput;
+    public Vector2 cameraInput;
+
+    public float cameraInputX;
+    public float cameraInputY;
+
+    private float moveAmount;
+    public float verticalInput;
+    public float horizontalInput;
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new DefaultInput();
+
+            playerControls.Player.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+            playerControls.Player.View.performed += i => cameraInput = i.ReadValue<Vector2>();
+        }
+
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+        //HandleJumpInput
+    }
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+
+        cameraInputY = cameraInput.y;
+        cameraInputX = cameraInput.x;
+
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
+    }
+}
