eb2d0afe814743c55b76d8d02ccd41c13839044b
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-09 20:41:54 +0300

    fix: Added defaults to the player settings.

--- Assets/Scripts/PlayerModels.cs
+++ Assets/Scripts/PlayerModels.cs
@@ -10,35 +10,35 @@ public static class PlayerModels
     {
         [Header("View Settings")]
         [Tooltip("Looking down/up sensitivity")]
-        public float ViewXSensitivity;
+        public float ViewXSensitivity = 6;
         [Tooltip("Looking left/right sensitivity")]
-        public float ViewYSensitivity;
+        public float ViewYSensitivity = 6;
         [Tooltip("Invert down/up?")]
-        public bool ViewXInverted;
+        public bool ViewXInverted = false;
         [Tooltip("Invert left/right?")]
-        public bool ViewYInverted;
+        public bool ViewYInverted = false;
 
         [Header("Movement Settings")]
         [Tooltip("Smooth movement amount")]
-        public float MovementSmoothing;
+        public float MovementSmoothing = 0.15f;
 
         [Header("Movement - Running")]
         [Tooltip("Forward running speed")]
-        public float RunningForwardSpeed;
+        public float RunningForwardSpeed = 8;
         [Tooltip("Left/right running speed")]
-        public float RunningStrafeSpeed;
+        public float RunningStrafeSpeed = 6;
 
         [Header("Movement - Walking")]
         [Tooltip("Forward walking speed")]
-        public float WalkingForwardSpeed;
+        public float WalkingForwardSpeed = 3;
         [Tooltip("Left/right walking speed")]
-        public float WalkingStrafeSpeed;
+        public float WalkingStrafeSpeed = 2;
 
         [Header("Jumping")]
         [Tooltip("Amount of force added to jumping")]
-        public float JumpingHeight;
+        public float JumpingHeight = 5;
         [Tooltip("Amount of time to smooth the jumping")]
-        public float JumpingFalloff;
+        public float JumpingFalloff = 0.15f;
     }
     [Serializable]
     public class CharacterStance
