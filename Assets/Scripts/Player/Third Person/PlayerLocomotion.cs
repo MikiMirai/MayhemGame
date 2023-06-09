344c612d4847a4932cdf12612aa5f9339981df21
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-09 22:30:31 +0300

    feat: Adding new player movement script.

---
+++ Assets/Scripts/Player/Third Person/PlayerLocomotion.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    InputManager inputManager;
+
+
+    Vector3 moveDirection;
+    Transform cameraObject;
+    Rigidbody playerRigidbody;
+
+    public float movementSpeed = 7;
+    public float rotationSpeed = 15;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+
+    public void HandleAllMovement()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection = moveDirection * movementSpeed;
+
+        Vector3 movementVelocity = moveDirection;
+        playerRigidbody.velocity = movementVelocity;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+}
