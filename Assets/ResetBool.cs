d769e5c393a66ce56624dd1927a2082da64d829f
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-10 02:01:56 +0300

    feat: Added animation resetters.

---
+++ Assets/ResetBool.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ResetBool : StateMachineBehaviour
+{
+    public string isInteractingBool;
+    public bool isInteractingStatus;
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(isInteractingBool, isInteractingStatus);
+    }
+}
