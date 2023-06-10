68584190bd455eef5d39d23f1d290069e0b830b6
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-10 13:25:36 +0300

    feat: Also reset root movement bool.

--- Assets/ResetBool.cs
+++ Assets/ResetBool.cs
@@ -4,9 +4,14 @@ public class ResetBool : StateMachineBehaviour
 {
     public string isInteractingBool;
     public bool isInteractingStatus;
+
+    public string isUsingRootMotionBool;
+    public bool isUsingRootMotionStatus;
+
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.SetBool(isInteractingBool, isInteractingStatus);
+        animator.SetBool(isUsingRootMotionBool, isUsingRootMotionStatus);
     }
 }
