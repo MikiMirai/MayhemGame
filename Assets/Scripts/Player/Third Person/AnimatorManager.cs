e966e8d88303c6636c037c952303fb0527fdadbb
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-10 13:24:32 +0300

    feat: The ability for animations to move player.

--- Assets/Scripts/Player/Third Person/AnimatorManager.cs
+++ Assets/Scripts/Player/Third Person/AnimatorManager.cs
@@ -3,19 +3,24 @@ using UnityEngine;
 public class AnimatorManager : MonoBehaviour
 {
     public Animator animator;
+    PlayerManager playerManager;
+    PlayerLocomotion playerLocomotion;
     int horizontal;
     int vertical;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        playerManager = GetComponent<PlayerManager>();
+        playerLocomotion = GetComponent<PlayerLocomotion>();
         horizontal = Animator.StringToHash("Horizontal");
         vertical = Animator.StringToHash("Vertical");
     }
 
-    public void PlayTargetAnimation(string targetAnimation, bool isInteracting)
+    public void PlayTargetAnimation(string targetAnimation, bool isInteracting, bool useRootMotion = false)
     {
         animator.SetBool("isInteracting", isInteracting);
+        animator.SetBool("isUsingRootMotion", useRootMotion);
         animator.CrossFade(targetAnimation, 0.2f);
     }
 
@@ -78,5 +83,17 @@ public class AnimatorManager : MonoBehaviour
 
         animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
         animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
+    }
+
+    private void OnAnimatorMove()
+    {
+        if (playerManager.isUsingRootMotion)
+        {
+            playerLocomotion.playerRigidbody.drag = 0;
+            Vector3 deltaPosition = animator.deltaPosition;
+            deltaPosition.y = 0;
+            Vector3 velocity = deltaPosition / Time.deltaTime;
+            playerLocomotion.playerRigidbody.velocity = velocity;
+        }
     }
 }
