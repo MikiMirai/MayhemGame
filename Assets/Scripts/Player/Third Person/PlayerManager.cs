7946f75eef9697f79e96edbcef14ad6ad3ac40e9
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-06-10 13:25:14 +0300

    feat: The ability for animation to move player.

--- Assets/Scripts/Player/Third Person/PlayerManager.cs
+++ Assets/Scripts/Player/Third Person/PlayerManager.cs
@@ -8,6 +8,7 @@ public class PlayerManager : MonoBehaviour
     PlayerLocomotion playerLocomotion;
 
     public bool isInteracting;
+    public bool isUsingRootMotion;
 
     private void Awake()
     {
@@ -42,6 +43,7 @@ public class PlayerManager : MonoBehaviour
         cameraManager.HandleAllCameraMovement();
 
         isInteracting = animator.GetBool("isInteracting");
+        isUsingRootMotion = animator.GetBool("isUsingRootMotion");
         playerLocomotion.isJumping = animator.GetBool("isJumping");
         animator.SetBool("isGrounded", playerLocomotion.isGrounded);
     }
