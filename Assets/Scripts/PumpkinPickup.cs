142397052963252c7c634479a2dfbca104afb1d5
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-11-07 19:04:15 +0200

    fix: collected pumpkins are saved to the player save

--- Assets/Scripts/PumpkinPickup.cs
+++ Assets/Scripts/PumpkinPickup.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
 
-public class PumpkinPickup : MonoBehaviour
+public class PumpkinPickup : MonoBehaviour, IDataPersistence
 {
+    [SerializeField] private string id;
+
+    [ContextMenu("Generate guid for id")]
+    private void GenerateGuid()
+    {
+        id = System.Guid.NewGuid().ToString();
+    }
+
     public int value;
+
+    private bool collected;
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (!collected && other.tag == "Player")
         {
-            FindObjectOfType<GameManager>().AddPumpkin(value);
-
-            Destroy(gameObject);
+            CollectPupmkin();
         }
+    }
+
+    private void CollectPupmkin()
+    {
+        FindObjectOfType<GameManager>().AddPumpkin(value);
+        collected = true;
+
+        Destroy(gameObject);
+    }
+
+    public void LoadData(GameData data)
+    {
+        data.pumpkinsCollected.TryGetValue(id, out collected);
+        if (collected)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        if (data.pumpkinsCollected.ContainsKey(id))
+        {
+            data.pumpkinsCollected.Remove(id);
+        }
+        data.pumpkinsCollected.Add(id, collected);
     }
 }
