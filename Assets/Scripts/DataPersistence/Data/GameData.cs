bdd09e8d07d312bc1a9da144d9201a6e3c126d04
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2023-11-07 19:00:57 +0200

    feat: added more things to be saved
    Added death count, player position and pumpkins collected (for testing pickups that need to disappear).

--- Assets/Scripts/DataPersistence/Data/GameData.cs
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 [System.Serializable]
 public class GameData 
 {
-    public int CarriedAmmo;
+    public int deathCount;
 
-    public int MagazineAmmo;
+    public int carriedAmmo;
 
+    public int magazineAmmo;
+
+    public Vector3 playerPosition;
+
+    public Dictionary<string, bool> pumpkinsCollected;
+
+    // The value defined in the constructor will be the default values
+    // The game will start with when there is no save files
     public GameData()
     {
-        this.CarriedAmmo = 0;
-        this.MagazineAmmo = 0;
+        this.deathCount = 0;
+        this.carriedAmmo = 0;
+        this.magazineAmmo = 0;
+        this.playerPosition = Vector3.zero; //TODO: Change this to the defualt map starting point
+        this.pumpkinsCollected = new Dictionary<string, bool>();
     }
 }
